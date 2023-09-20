323caefc652839697753aeac00e6f30e43ab4e8c
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-09-20 21:37:37 +0200

    user schema for mongoDB + bCrypt'ing

---
+++ backend/schemas/user.schema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bCrypt = require("bcryptjs");
+
+const UserSchema = new Schema({
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    token: {
+        type: String,
+        default: null,
+    },
+})
+
+UserSchema.methods.setPassword = function (password) {
+    this.password = bCrypt.hashSync(password, bCrypt.genSaltSync(6));
+};
+
+UserSchema.methods.validPassword = function (password) {
+    return bCrypt.compareSync(password, this.password);
+};
+
+
+const User = mongoose.model('users', UserSchema);
+module.exports = User;
