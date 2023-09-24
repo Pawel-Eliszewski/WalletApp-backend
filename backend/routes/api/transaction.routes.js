0b338b5b29b59332cd5311b0dc19012baa893605
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-09-24 08:46:08 +0200

    added get transaction route by its id

--- backend/routes/api/transaction.routes.js
+++ backend/routes/api/transaction.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 require('dotenv').config()
 const auth = require("../../middlewares/auth");
 const {addTransaction, getTransactionById} = require('../../controllers/transaction.controller')
+
 router.post('/', auth, async (req, res, next) => {
     try {
         const {type, category, amount, date, comment, owner} = req.body;
@@ -12,6 +13,28 @@ router.post('/', auth, async (req, res, next) => {
             code: 200,
             data: createdTransaction._id
         })
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get('/category/:transactionId', auth, async (req, res, next) => {
+    try {
+        const {transactionId} = req.query;
+        const transaction = await getTransactionById(transactionId);
+        if (transaction) {
+            res.json({
+                status: 'Success',
+                code: 200,
+                data: transaction
+            })
+        } else {
+            res.json({
+                status: "Not found",
+                code: 404,
+                message: "Given transaction does not exist"
+            })
+        }
     } catch (err) {
         next(err);
     }
