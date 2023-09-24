2187be7f4220db46d342b9506fe2dfb8158589b6
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-09-24 17:17:05 +0200

    changed route to /user

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use('/api/users', usersRouter);
+app.use('/api/user', usersRouter);
 app.use('/api/transaction', transactionsRouter);
 
 app.use(function (req, res, next) {
