644028b2c69cf0069a6273e39e01127de2cff679
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-09-26 12:58:55 +0200

    added function responsible for fetching user transactions, sorting them by date, and calculating sum of income and expense

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -12,8 +12,31 @@ const getUsersTransactions = async (userId) => {
     return Transaction.find({userId});
 }
 
+const getUserStatisticsByDate = async (userId, transactionsDate) => {
+    const transactions = await getUsersTransactions(userId);
+    const transactionsFilteredByDate = transactions.filter(transaction => {
+        const transactionDate = new Date(transaction.date);
+        const transactionYear = transactionDate.getFullYear();
+        const transactionMonth = transactionDate.getMonth() + 1;
+        const [year, month] = transactionsDate.split('-').map(Number);
+        return transactionYear === year && transactionMonth === month;
+    });
+    let sumOfIncome = 0;
+    let sumOfExpense = 0;
+
+    for (const transaction of transactionsFilteredByDate) {
+        if (transaction.type === 'income') {
+            sumOfIncome += transaction.amount;
+        } else if (transaction.type === 'expense') {
+            sumOfExpense += transaction.amount;
+        }
+    }
+    return [transactionsFilteredByDate, sumOfIncome, sumOfExpense];
+}
+
 module.exports = {
     addTransaction,
     getTransactionById,
-    getUsersTransactions
+    getUsersTransactions,
+    getUserStatisticsByDate,
 }
