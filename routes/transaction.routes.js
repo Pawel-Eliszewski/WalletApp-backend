ca965b5dc6e00f84f0b8050d3f2a078b97335191
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-10-02 13:21:35 +0200

    fix for patch

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -247,11 +247,12 @@ router.patch('/:transactionId', auth, async (req, res, next) => {
         const {transactionId} = req.params;
         const { type, category, amount, date, comment, owner} = req.body;
         const updateResult = await updateTransaction(transactionId, type, category, amount, date, comment, owner);
-        if (updateResult.nModified > 0) {
+        if (updateResult) {
             res.json({
                 status: 'Success',
                 code: 200,
-                message: 'Transaction updated successfully'
+                message: 'Transaction updated successfully',
+                data: updateResult
             })
         } else {
             res.json({
